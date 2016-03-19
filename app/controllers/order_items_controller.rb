69891a02742c2bd2cc943251533ef3929f41fc00
giriraj0209 <giriraj.kashyapa@gmail.com>
2016-03-18 22:53:45 -0500

    Created Ajax calls for the cart update

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,11 +1,25 @@
 class OrderItemsController < ApplicationController
   def create
-  	
+  	logger.info "*********************************Im in create***********************************************"
+	@order = current_order
+    @order_item = @order.order_items.new(order_item_params)
+    @order.save
+    session[:order_id] = @order.id
   end
 
   def update
+  	logger.info "*********************************Im in update***********************************************"
+  	@order = current_order
+    @order_item = @order.order_items.find(params[:id])
+    @order_item.update_attributes(order_item_params)
+    @order_items = @order.order_items
   end
 
   def destroy
+  end
+
+  private
+  def order_item_params
+    params.require(:order_item).permit(:quantity, :product_id)
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
+  	session[:order_id]=nil
     @products = Product.all
     @order_item = current_order.order_items.new
   end
